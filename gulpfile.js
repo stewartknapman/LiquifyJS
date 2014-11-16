9cafa8fe2ec6e47bbf8f06b2c665ff5525b2ca9f
Stewart Knapman <info@stewartknapman.com>
2014-11-16 15:25:01 +1300

    get example compiling

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var gulp        = require('gulp'),
   plumber       = require('gulp-plumber'),
   source        = require('vinyl-source-stream'),
   browserify    = require('browserify'),
-  jshint        = require('gulp-jshint');
+  jshint        = require('gulp-jshint'),
+  sass          = require('gulp-ruby-sass'),
+  prefix        = require('gulp-autoprefixer');
   
 var js_files = ['./src/*.js', './src/**/*.js'];
   
@@ -12,6 +14,9 @@ var onError = function (err) {
   gutil.log(gutil.colors.red(err));
 };
 
+/*
+  Build
+*/
 gulp.task('default', function () {
   gulp.watch(js_files, ['lint', 'browserify']);
 });
@@ -42,4 +47,41 @@ gulp.task('build', function () {
     .bundle()
     .pipe(source('pager.js'))
     .pipe(gulp.dest('./build/'));
+});
+
+/*
+  Example
+*/
+gulp.task('example', function () {
+  gulp.watch(['./example/app.js', './example/style.scss'], ['lint_example', 'browserify_example', 'sass_example']);
+});
+
+gulp.task('lint_example', function () {
+  return gulp.src('./example/app.js')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('browserify_example', function () {
+  return browserify('./example/app.js', {
+      debug: true
+    })
+    .bundle()
+    .pipe(source('app.browserifed.js'))
+    .pipe(gulp.dest('./example/'));
+});
+
+gulp.task('sass_example', function () {
+  return gulp.src('./example/style.scss')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(sass({
+      style: 'compact'
+    }))
+    .pipe(prefix("last 2 versions", "> 5%", "ie 8", "ie 7"))
+    .pipe(gulp.dest('./example/'));
 });
