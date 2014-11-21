0c5ce98bb2765d37d14d0fc4a66fe36b0555efb2
Stewart Knapman <info@stewartknapman.com>
2014-11-22 12:58:23 +1300

    defaults and settings

--- test/liquify_spec.js
+++ test/liquify_spec.js
@@ -9,13 +9,14 @@ describe('LiquifyJS', function () {
     template_1: 'hello {{ name }}'
   };
 
+  // Geting started
   it('returns the liquify object when instatiated', function () {
     // check something funky is not going on, ie errors
     var liquify = new Liquify();
     expect(typeof liquify).toBe('object');
   });
 
-  it('it accepts an optional object of templates on instantiation', function () {
+  it('accepts an optional object of templates on instantiation', function () {
     var liquify = new Liquify({
       templates: templates
     });
@@ -23,7 +24,7 @@ describe('LiquifyJS', function () {
     expect(liquify.templates.template_1).toBe('hello {{ name }}');
   });
 
-  it('it accepts an optional data on instantiation', function () {
+  it('accepts an optional data on instantiation', function () {
     var liquify = new Liquify({
       templates: templates,
       data: {
@@ -32,6 +33,35 @@ describe('LiquifyJS', function () {
     });
     
     expect(liquify.data.name).toBe('liquify');
+  });
+  
+  // Default settings
+  it('has a sensible set of default settings', function () {
+    expect(Liquify.Defaults.active_class).toBe('is-active');
+    expect(Liquify.Defaults.data_attrs.target).toBe('data-lqfy-target');
+    expect(Liquify.Defaults.data_attrs.template).toBe('data-lqfy-template');
+  });
+  
+  it('uses these default settings', function () {
+    var liquify = new Liquify();
+    expect(liquify.settings.active_class).toBe('is-active');
+    expect(liquify.settings.data_attrs.target).toBe('data-lqfy-target');
+    expect(liquify.settings.data_attrs.template).toBe('data-lqfy-template');
+  });
+  
+  it('can have these default settings overridden', function () {
+    var liquify = new Liquify({
+      settings: {
+        active_class: 'is-current',
+        data_attrs: {
+          target: 'data-nav-target',
+          template: 'data-nav-template'
+        }
+      }
+    });
+    expect(liquify.settings.active_class).toBe('is-current');
+    expect(liquify.settings.data_attrs.target).toBe('data-nav-target');
+    expect(liquify.settings.data_attrs.template).toBe('data-nav-template');
   });
   
 });
